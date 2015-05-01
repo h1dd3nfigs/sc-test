8bc6d7eb45e9b2b8b5f52f44a5e157bb81f232da
Ruth Fombrun <rfombrun@gmail.com>
2015-05-01 16:17:32 -0400

    Completed question 2

--- q2-2.php
+++ q2-2.php
@@ -1,32 +1,57 @@
 <?php
 
-$inputs = array(1, 1, 3, 5, 6, 8, 10, 11, 10, 9, 8, 9, 10, 11, 7);
-
-$deltas = array();
-
-for($i = 0; $i < count($inputs)-1; $i++){
-	$deltas[] = $inputs[$i + 1] - $inputs[$i];
-}
-
 function isOne($num)
 {
-	if(	abs($num)== 1){
+	if(	abs($num)=== 1)
+	{
 		return true;
 	}
 	return false;
 }
 
-$filtered_deltas = array_filter($deltas, 'isOne');
-$deltas_keys = array_keys($deltas);
+function consecutiveNumIndices($inputs)
+{
+	$deltas = array();
+	$remove_keys = array();
 
-// start pointer at 2nd indexed position
-next($deltas_keys);
-next($deltas);
-
-//foreach($deltas as $delta){	
-	if( current($filtered_deltas) == prev($filtered_deltas) and current($deltas_keys) - 1 == prev($deltas_keys) ){
-		unset($filtered_deltas);
+	for($i = 0; $i < count($inputs)-1; $i++)
+	{
+		$deltas[] = $inputs[$i + 1] - $inputs[$i];
 	}
-//}
 
-print_r($filtered_deltas);
+	$filtered_deltas = array_filter($deltas, 'isOne');
+	
+	if(count($filtered_deltas)=== 0)
+	{	
+		return null;
+	}
+
+	$deltas_keys = array_keys($filtered_deltas);
+
+	while(current($deltas_keys) !== False)
+	{
+		$curr_key = current($deltas_keys);
+		$next_key = next($deltas_keys);
+
+		$curr_delta = current($filtered_deltas);
+		$next_delta = next($filtered_deltas);
+		
+		if($next_delta === $curr_delta and 
+			$next_key - $curr_key === 1)
+		{
+			$remove_keys[] = key($deltas_keys);
+		}	
+	}
+	
+	foreach($remove_keys as $key){
+		unset($deltas_keys[$key]);
+	}
+
+	return $deltas_keys;
+
+
+}
+
+$inputs = array(1, 1, 3, 5, 6, 8, 10, 11, 10, 9, 8, 9, 10, 11, 7);
+$positions = consecutiveNumIndices($inputs);
+var_dump($positions);
